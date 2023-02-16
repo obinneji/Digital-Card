bbbcae02b576bf02fcfbe05f4998da7332686cfb
Obinneji Chibuzor Francis <87784578+obinneji@users.noreply.github.com>
2023-02-16 18:48:26 +0100

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -10,7 +10,7 @@ const Info = () => {
       <h2 className='user-name'>Obinneji Chibuzor</h2>
       <p className='user-title'>Frontend Developer</p>
       <p className='user-website'>ObinnejiChibuzor.com</p>
-      <a href='mailto:obinnnejic@gmail.com'><button className='email-btn'><FaEnvelope className='contact-icon'/>Email</button></a>
+      <a href='mailto:obinnejic@gmail.com'><button className='email-btn'><FaEnvelope className='contact-icon'/>Email</button></a>
       <a href='https://www.linkedin.com/in/obinneji/'><button className='linkedin-btn'><FaLinkedin className='contact-icon'/>LinkedIn</button></a>
     </header>
   )
